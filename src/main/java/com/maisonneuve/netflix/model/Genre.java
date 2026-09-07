305d9f5b3bc07219195949a41629c5c0b77ec6fd
Fadhel-Smari <fadhelsmari@gmail.com>
2026-09-07 18:13:10 -0400

    Ajout enum Genre

---
+++ src/main/java/com/maisonneuve/netflix/model/Genre.java
@@ -0,0 +1,19 @@
+package com.maisonneuve.netflix.model;
+
+public enum Genre {
+    ACTION,
+    ANIMATION,
+    BIOGRAPHIE,
+    COMEDIE,
+    CRIME,
+    DOCUMENTAIRE,
+    DRAME,
+    FAMILLE,
+    FANTASTIQUE,
+    GUERRE,
+    HORREUR,
+    MUSICAL,
+    ROMANCE,
+    SCIENCE_FICTION,
+    THRILLER
+}
